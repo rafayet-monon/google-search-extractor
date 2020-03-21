6a87de66a3cbe01533fa5481b73d6d23f1dad9af
rafayet <rafayet@hashkloud.com>
2020-03-21 21:06:53 +0600

    Malformed CSV error handled.

--- app/models/search_file.rb
+++ app/models/search_file.rb
@@ -3,15 +3,11 @@ class SearchFile < ApplicationRecord
   belongs_to :user, dependent: :destroy
   has_many :search_results
 
-  enum status: { initialized: 0, running: 1, retring: 2, completed: 3, failed: 4 }
+  enum status: { initialized: 0, running: 1, completed: 2, failed: 3 }
 
   private
 
   def start_search_worker
-    if Rails.application.credentials.dig(:heroku, Rails.env.to_sym).present?
-      FileProcessingWorker.new.perform(id)
-    else
-      FileProcessingWorker.perform_async(id)
-    end
+    FileProcessingWorker.perform_async(id)
   end
 end
